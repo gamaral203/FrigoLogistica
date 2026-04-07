932e4b084d7223ca0dbd9d91a9ab9af6747b332d
Gabriel amaral <amaralgabriel357@gmail.com>
2026-04-07 19:06:19 -0300

    feat: cria os DTOs e o mapper para retirar  a responsabilidade do model

--- src/main/java/com/frigocezar/logistica/dto/VeiculoDTO.java
+++ src/main/java/com/frigocezar/logistica/dto/VeiculoDTO.java
@@ -1,4 +1,32 @@
 package com.frigocezar.logistica.dto;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class VeiculoDTO {
+
+    private Long id;
+
+
+    private String tipoVeiculo;
+
+
+    private String placa;
+
+
+    private String marca;
+
+
+    private String modelo;
+
+
+    private String renvam;
+
+
+    private String cor;
 }
--- src/main/java/com/frigocezar/logistica/mapper/VeiculoMapper.java
+++ src/main/java/com/frigocezar/logistica/mapper/VeiculoMapper.java
@@ -1,4 +1,33 @@
 package com.frigocezar.logistica.mapper;
 
+import com.frigocezar.logistica.dto.VeiculoDTO;
+import com.frigocezar.logistica.model.VeiculoModel;
+import org.springframework.stereotype.Component;
+
+@Component
 public class VeiculoMapper {
+
+    public VeiculoModel map(VeiculoDTO veiculoDTO) {
+        VeiculoModel veiculoModel = new VeiculoModel();
+        veiculoModel.setId(veiculoDTO.getId());
+        veiculoModel.setTipoVeiculo(veiculoDTO.getTipoVeiculo());
+        veiculoModel.setMarca(veiculoDTO.getMarca());
+        veiculoModel.setPlaca(veiculoDTO.getPlaca());
+        veiculoModel.setModelo(veiculoDTO.getModelo());
+        veiculoModel.setCor(veiculoDTO.getCor());
+        veiculoModel.setRenvam(veiculoDTO.getRenvam());
+        return veiculoModel;
+    }
+
+    public VeiculoDTO map(VeiculoModel veiculoModel) {
+        VeiculoDTO veiculoDTO = new VeiculoDTO();
+        veiculoDTO.setId(veiculoModel.getId());
+        veiculoDTO.setTipoVeiculo(veiculoModel.getTipoVeiculo());
+        veiculoDTO.setMarca(veiculoModel.getMarca());
+        veiculoDTO.setPlaca(veiculoModel.getPlaca());
+        veiculoDTO.setModelo(veiculoModel.getModelo());
+        veiculoDTO.setCor(veiculoModel.getCor());
+        veiculoDTO.setRenvam(veiculoModel.getRenvam());
+        return veiculoDTO;
+    }
 }
