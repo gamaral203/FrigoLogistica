2ef23159b03d3d0b7f146cd107fc3f18fabeedfd
Gabriel amaral <amaralgabriel357@gmail.com>
2026-05-07 10:30:10 -0300

    refactor: refatora a classes MotoristaController e MotoristaSevice, aplicando as melhores práticas e enxugando a classe

--- src/main/java/com/frigocezar/logistica/service/MotoristaService.java
+++ src/main/java/com/frigocezar/logistica/service/MotoristaService.java
@@ -63,27 +63,32 @@ public class MotoristaService {
 
     public MotoristaDTO atualizarMotoristaPorId(Long id, MotoristaDTO motoristaDTO) {
         log.debug("Atualizando motorista por id: {}", id);
-        Optional<MotoristaModel> motoristaExistente = motoristaRepository.findById(id);
-        if (motoristaExistente.isPresent()) {
-            MotoristaModel motorista = motoristaExistente.get();
-            motorista.setNome(motoristaDTO.getNome());
-            motorista.setCnh(motoristaDTO.getCnh());
-            motorista.setCpf(motoristaDTO.getCpf());
 
-            MotoristaModel motoristaAtualizado = motoristaRepository.save(motorista);
+        MotoristaModel motorista = motoristaRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.warn("Motorista não encontrado. id: {}", id);
+                    return new MotoristaNotFoundException();
+                });
 
-            log.info("Motorista atualizado com sucesso. id: {}", id);
+        motorista.setNome(motoristaDTO.getNome());
+        motorista.setCnh(motoristaDTO.getCnh());
+        motorista.setCpf(motoristaDTO.getCpf());
 
-            return motoristaMapper.map(motoristaAtualizado);
-        } else {
-            log.warn("Motorista não encontrado. id: {}", id);
-            throw new MotoristaNotFoundException();
-        }
+        MotoristaModel motoristaAtualizado = motoristaRepository.save(motorista);
+
+        log.info("Motorista atualizado com sucesso. id: {}", id);
+
+        return motoristaMapper.map(motoristaAtualizado);
     }
 
     // Deletar Por Id
     public void deletarMotoristaPorId(Long id) {
         log.debug("Deletando motorista por id: {}", id);
+        MotoristaModel motorista = motoristaRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.warn("Motorista não encontrado. id: {}", id);
+                    return new MotoristaNotFoundException();
+                });
         motoristaRepository.deleteById(id);
         log.info("Motorista deletado com sucesso. id: {}", id);
     }
