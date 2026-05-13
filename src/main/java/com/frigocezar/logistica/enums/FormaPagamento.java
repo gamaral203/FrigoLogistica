a53d6c3379a1f22c359c1399b29281ad9ae723df
Gabriel amaral <amaralgabriel357@gmail.com>
2026-05-13 18:20:41 -0300

    feat: cria enums para despesas

---
+++ src/main/java/com/frigocezar/logistica/enums/FormaPagamento.java
@@ -0,0 +1,9 @@
+package com.frigocezar.logistica.enums;
+
+public enum FormaPagamento {
+    DINHEIRO,
+    PIX,
+    CARTAO_CREDITO,
+    CARTAO_DEBITO,
+    BOLETO
+}
---
+++ src/main/java/com/frigocezar/logistica/enums/StatusDespesa.java
@@ -0,0 +1,8 @@
+package com.frigocezar.logistica.enums;
+
+public enum StatusDespesa {
+    PENDENTE,
+    APROVADA,
+    REEMBOLSADA,
+    CANCELADA
+}
