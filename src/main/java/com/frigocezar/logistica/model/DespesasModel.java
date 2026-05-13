6c837e2efd404d5f300adeddfbeb27c697c165e9
Gabriel amaral <amaralgabriel357@gmail.com>
2026-05-13 13:02:13 -0300

    feat: inicia a modelagem das despesas

---
+++ src/main/java/com/frigocezar/logistica/model/DespesasModel.java
@@ -0,0 +1,23 @@
+package com.frigocezar.logistica.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_despesas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DespesasModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descricao;
+
+}
