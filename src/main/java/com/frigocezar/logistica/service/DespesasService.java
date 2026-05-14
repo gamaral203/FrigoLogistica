575404e1ec81838ea4595fffbb732fc0a3adf40f
Gabriel amaral <amaralgabriel357@gmail.com>
2026-05-14 14:25:03 -0300

    feat: cria uma busca por id no service

--- src/main/java/com/frigocezar/logistica/service/DespesasService.java
+++ src/main/java/com/frigocezar/logistica/service/DespesasService.java
@@ -1,6 +1,8 @@
 package com.frigocezar.logistica.service;
 
 import com.frigocezar.logistica.dto.DespesasDTO;
+import com.frigocezar.logistica.dto.MotoristaDTO;
+import com.frigocezar.logistica.exceptions.DespesasNotFoundException;
 import com.frigocezar.logistica.mapper.DespesasMapper;
 import com.frigocezar.logistica.model.DespesasModel;
 import com.frigocezar.logistica.repository.DespesasRepository;
@@ -39,5 +41,16 @@ public class DespesasService {
         return despesas.stream()
                 .map(despesasMapper::map)
                 .collect(Collectors.toList());
+    }
+
+    public DespesasDTO buscarDespesa(Long id) {
+        log.debug("Buscando despesa com id {}", id);
+        DespesasModel despesasModel = despesasRepository.findById(id)
+                .orElseThrow(() -> {
+                    log.info("Despesa não encontrada {}", id);
+                    return new DespesasNotFoundException();
+                });
+        log.info("Despesa encontrada, id: {} ", id);
+        return despesasMapper.map(despesasModel);
     }
 }
