d731758ec20dc0dab49b4343fe2b538017e14c2d
Gabriel amaral <amaralgabriel357@gmail.com>
2026-06-20 09:39:13 -0300

    hotfix: resolve bug de retorno 500, fazendo o set da quantidade do estoque e tipo do produto

--- src/main/java/com/frigocezar/logistica/mapper/ProdutoMapper.java
+++ src/main/java/com/frigocezar/logistica/mapper/ProdutoMapper.java
@@ -2,6 +2,7 @@ package com.frigocezar.logistica.mapper;
 
 import com.frigocezar.logistica.dto.ProdutoDTO;
 import com.frigocezar.logistica.model.ProdutoModel;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,6 +14,8 @@ public class ProdutoMapper {
         produtoModel.setNome(produtoDTO.getNome());
         produtoModel.setDescricao(produtoDTO.getDescricao());
         produtoModel.setPreco(produtoDTO.getPreco());
+        produtoModel.setQuantidadeEstoque(produtoDTO.getQuantidadeEstoque());
+        produtoModel.setTipoProduto(produtoDTO.getTipoProduto());
         return produtoModel;
     }
     public ProdutoDTO map(ProdutoModel produtoModel) {
@@ -21,6 +24,8 @@ public class ProdutoMapper {
         produtoDTO.setNome(produtoModel.getNome());
         produtoDTO.setDescricao(produtoModel.getDescricao());
         produtoDTO.setPreco(produtoModel.getPreco());
+        produtoDTO.setTipoProduto(produtoModel.getTipoProduto());
+        produtoDTO.setQuantidadeEstoque(produtoModel.getQuantidadeEstoque());
         return produtoDTO;
     }
 }
